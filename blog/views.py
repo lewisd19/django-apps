1213106e0290ed800a150efe7df10bf7fafa93e8
d-nice <d-nice@d-nices-mac.dnice.local>
2019-06-13 13:13:33 -0400

    first commit

---
+++ blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, get_object_or_404
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
